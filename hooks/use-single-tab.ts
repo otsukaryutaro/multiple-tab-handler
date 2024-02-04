6d5cbd9d55a4ba3a322e23c1867f51ad90a80632
大塚　竜太郎 <otsukaryutaro@team-lab.com>
2024-02-04 17:35:00 +0900

    use-single-tab

---
+++ hooks/use-single-tab.ts
@@ -0,0 +1,36 @@
+import { useEffect, useRef } from 'react';
+
+export const useSingleTab = () => {
+  // 初期表示が成功したかをフラグで管理
+  const flag = useRef(false);
+  useEffect(() => {
+    const currentPath = window.location.pathname;
+    const storedPath = localStorage.getItem('storedPath');
+
+    if (storedPath === currentPath) {
+      throw new Error('Same page');
+    }
+
+    localStorage.setItem('storedPath', currentPath);
+    // 初期表示が成功
+    flag.current = true;
+
+    return () => {
+      localStorage.removeItem('storedPath');
+    };
+  }, []);
+
+  useEffect(() => {
+    const handler = () => {
+      // リロードやタブを閉じるときに実行される
+      // ただし、初期表示で失敗した場合は実行されない
+      if (flag.current) {
+        localStorage.removeItem('storedPath');
+      }
+    };
+    window.addEventListener('beforeunload', handler);
+    return () => {
+      window.removeEventListener('beforeunload', handler);
+    };
+  }, []);
+};

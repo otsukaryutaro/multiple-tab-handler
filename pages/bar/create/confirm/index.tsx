12214c16429e2416152a9dd877cbfd4ff7ad5be1
大塚　竜太郎 <otsukaryutaro@team-lab.com>
2024-02-04 17:24:10 +0900

    confirm

--- pages/bar/create/confirm/index.tsx
+++ pages/bar/create/confirm/index.tsx
@@ -1,15 +1,24 @@
 import Link from 'next/link';
 import { useRecoilValue } from 'recoil';
 import { formInputState } from '../../../../atoms/form-input';
+import { useRouter } from 'next/router';
 
 export default function BarCreateConfirm() {
+  const router = useRouter();
   const data = useRecoilValue(formInputState);
+
+  const handleConfirm = () => {
+    console.log({ data });
+    router.push('/bar/complete');
+  };
 
   return (
     <div style={{ display: 'flex', flexDirection: 'column' }}>
       <h1>Bar create confirm page!</h1>
       <p>{JSON.stringify(data)}</p>
-      <Link href="/bar/complete">Bar complete</Link>
+      <button type="button" onClick={handleConfirm}>
+        Bar complete page
+      </button>
       <Link href="/bar">Bar page</Link>
     </div>
   );

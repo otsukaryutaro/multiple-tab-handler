c242831c15e1a9df19e9a37509a712612187a212
大塚　竜太郎 <otsukaryutaro@team-lab.com>
2024-02-04 17:09:36 +0900

    rfh

--- pages/bar/create/index.tsx
+++ pages/bar/create/index.tsx
@@ -1,11 +1,28 @@
 import Link from 'next/link';
+import { SubmitHandler, useForm } from 'react-hook-form';
+
+type Inputs = {
+  example: string;
+  exampleRequired: string;
+};
 
 export default function BarCreate() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
   return (
     <div style={{ display: 'flex', flexDirection: 'column' }}>
       <h1>Bar create page!</h1>
-      {/* // TODO: フォームでの遷移にする */}
-      <Link href="/bar/create/confirm">Bar create confirm page</Link>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input defaultValue="test" {...register('example')} />
+        <input {...register('exampleRequired', { required: true })} />
+        {errors.exampleRequired && <span>This field is required</span>}
+        <input type="submit" />
+      </form>
       <Link href="/bar">Bar page</Link>
     </div>
   );

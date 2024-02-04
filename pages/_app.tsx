3be2a10f6f2404e8c4c8f80de7a2fe9e7e12a42b
大塚　竜太郎 <otsukaryutaro@team-lab.com>
2024-02-04 16:59:48 +0900

    erro-boundary入れた、けどthrowしたエラーをキャッチできてない気がする

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,11 @@
-import type { AppProps } from 'next/app'
- 
+import type { AppProps } from 'next/app';
+import { ErrorBoundary } from 'react-error-boundary';
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  return (
+    <ErrorBoundary fallback={<div>Something went wrong</div>}>
+      <Component {...pageProps} />
+    </ErrorBoundary>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
-import Link from "next/link";
+import Link from 'next/link';
 
 export default function Page() {
+
   return (
-    <div style={{ display: 'flex', flexDirection:"column" }}>
+    <div style={{ display: 'flex', flexDirection: 'column' }}>
       <h1>Hello, Next.js!</h1>
       <Link href="/bar">Bar page</Link>
       <Link href="/foo">Foo page</Link>

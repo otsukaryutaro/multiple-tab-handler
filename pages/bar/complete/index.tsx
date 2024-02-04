2b18206c611042d3fdeb0af761c16b011b7453c1
大塚　竜太郎 <otsukaryutaro@team-lab.com>
2024-02-04 17:30:13 +0900

    resetしてみた

--- pages/bar/complete/index.tsx
+++ pages/bar/complete/index.tsx
@@ -1,6 +1,16 @@
 import Link from 'next/link';
+import { formInputState } from '../../../atoms/form-input';
+import { useResetRecoilState } from 'recoil';
+import { useEffect } from 'react';
 
 export default function BarCompletePage() {
+  // FIXME: ここでglobalに保存していた値をクリアする？よくわからない
+  const resetFormInputState = useResetRecoilState(formInputState);
+
+  useEffect(() => {
+    resetFormInputState();
+  }, []);
+
   return (
     <div style={{ display: 'flex', flexDirection: 'column' }}>
       <h1>Bar complete page!</h1>

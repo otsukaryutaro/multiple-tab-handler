39e82beaa1859d8e38ba48db8c922f5561cda28b
大塚　竜太郎 <otsukaryutaro@team-lab.com>
2024-02-04 17:00:14 +0900

    comment

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from 'next/app';
 import { ErrorBoundary } from 'react-error-boundary';
 
 export default function App({ Component, pageProps }: AppProps) {
+  // FIXME: throwしたエラーをキャッチする方法をちゃんと確認する
 
   return (
     <ErrorBoundary fallback={<div>Something went wrong</div>}>
